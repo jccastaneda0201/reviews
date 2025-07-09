c90c5eb05cdbbe73a5ef6abe4ae2f3b02c441ffa
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-09 17:44:53 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="button-container">
+          <button className="prev-btn" onClick={() => setIndex(index - 1)}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={() => setIndex(index + 1)}>
+            <FaChevronRight />
+          </button>
+        </div>
+      </article>
+    </main>
+  );
 };
 export default App;
